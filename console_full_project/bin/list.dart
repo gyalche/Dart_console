669dd7a9886b66e7402e25f8d53671a17ac81f46
Dawa <sherpadawa01@outlook.com>
2022-07-18 00:21:12 +0545

    worked on list

---
+++ console_full_project/bin/list.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  List<int> myList=[1,2,3,4];
+  // int ans=myList.length;
+  
+  print(ans);
+  final myList2=[1,2,3,4];
+
+  //OR;
+  <int>[1,2,3,4]
+}
--- console_full_project/bin/namedparameter.dart
+++ console_full_project/bin/namedparameter.dart
@@ -4,3 +4,13 @@ int named(int x,{int a=1, int b=2}){
 void main(List<String> args){
   print(named(1, a:5, b:5));
 }
+
+//required parameters;
+
+void namedRequiredParameters({
+  required int?x,
+  required double?y,
+  required  String greeting
+}){
+  
+}
