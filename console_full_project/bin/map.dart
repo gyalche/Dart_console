dee8852d5f306de70319f931daf1e09c263aee77
Dawa <sherpadawa01@outlook.com>
2022-07-19 01:24:13 +0545

    woring on map and filter

--- console_full_project/bin/map.dart
+++ console_full_project/bin/map.dart
@@ -1,10 +1,23 @@
-void main(List<String> args) {
-  Map<String, dynamic> myMap={
-    'name':'dawa',
-    'age':25,
-    'registerd':true,
-  };
+// void main(List<String> args) {
+//   Map<String, dynamic> myMap={
+//     'name':'dawa',
+//     'age':25,
+//     'registerd':true,
+//   };
 
-  final name=myMap['name'];
-  print(name);
+//   final name=myMap['name'];
+//   print(name);
+// }
+
+void main(List<String> args) {
+  final names=['dawa', 'kancha', 'chungda', 'tshering dai'];
+
+  // final nameLength=names.map((name)=>name.length).toList();
+  //print(nameLength[0]);
+
+  //FILTER;
+  final nameFiltered=names.where((name)=>name.length>4).toList();
+  print(nameFiltered);
+
+  
 }
